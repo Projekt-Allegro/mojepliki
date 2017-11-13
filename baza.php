f64dc894635288a5c757246206caaf47ca51d06b
szef2 <kiemoneiro@gmail.com>
2017-11-13 17:58:35 +0100

    Update baza.php

--- baza.php
+++ baza.php
@@ -1,71 +1,71 @@
 <?php header('Content-Type: text/html;charset=UTF-8');
 
-$conn = mysqli_connect('localhost', 'root', '');
-$datab = mysqli_select_db($conn ,'alegro');
+$conn = mysqli_connect('localhost', 'root', ''); //łączy z bazą
+$datab = mysqli_select_db($conn ,'komis'); // wybiera baze
 
 $select = "select *";
-$skad = "from produkty ";
+$skad = "from auto ";
 $gdzie = "where";
-$kategoria = $_POST["kat"];
+$kolor = $_POST["kat"];
 $cena = $_POST["cena"];
-$cena1 = $_POST["cena1"];
+$przebieg = $_POST["przebieg"];
 $co = $_POST["1"];
 
-$co = "'".mysqli_real_escape_string($conn, stripslashes($co))."'";
+$co = "'".mysqli_real_escape_string($conn, stripslashes($co))."'"; // dodaje automatycznie quoty do nazwy
 $array = array($co);
 $values = implode(", ", $array);
 
-if ($kategoria === "" && $co === "" && $cena1 === "") {
-ob_start();
+if ($kolor === "" && $co === "" && $przebieg === "") { // zasady działania dla pustych pól
+ob_start(); // zapisuje echo do stringa
 echo $select,' ', $skad;
 $all = ob_get_contents();
 ob_end_clean();
-}elseif ($kategoria === "" && $co === "") {
+}elseif ($kolor === "" && $co === "") {
 ob_start();
-echo $select,' ', $skad, $gdzie,' ', $cena,' ', $cena1;
+echo $select,' ', $skad, $gdzie,' ', $cena;
 $all = ob_get_contents();
 ob_end_clean();
-}elseif ($co === "" && $cena1 === ""){
+}elseif ($co === "" && $przebieg === ""){
 ob_start();
-echo $select,' ', $skad,$gdzie,' ', 'kategoria like ', $kategoria;
+echo $select,' ', $skad,$gdzie,' ', 'kolor like ', $kolor;
 $all = ob_get_contents();
 ob_end_clean();
-}elseif ($kategoria === "" && $cena1 === "") {
+}elseif ($kolor === "" && $przebieg === "") {
 ob_start();
-echo $select,' ', $skad, $gdzie,' ','nazwa like ',"$co";
+echo $select,' ', $skad, $gdzie,' ','marka like ',"$co";
 $all = ob_get_contents();
 ob_end_clean();
 }elseif ($co === ""){
 ob_start();
-echo $select,' ', $skad,$gdzie,' ', 'kategoria like ', $kategoria, ' and',' cena ', $cena,' ', $cena1;
+echo $select,' ', $skad,$gdzie,' ', 'kolor like ', $kolor, ' and ', $cena;
 $all = ob_get_contents();
 ob_end_clean();
-}elseif ($kategoria === "") {
+}elseif ($kolor === "") {
 	ob_start();
-echo $select,' ', $skad,$gdzie,' cena ', $cena,' ', $cena1, ' and ', 'nazwa like ',"$co";
+echo $select,' ', $skad,$gdzie,$cena,' and ', $przebieg, ' and ', 'marka like ',"$co";
 $all = ob_get_contents();
 ob_end_clean();
-}elseif ($cena1 === ""){
+}elseif ($przebieg === ""){
 ob_start();
- echo $select,' ', $skad,$gdzie, ' ','kategoria like ',$kategoria ,' and ', 'nazwa like ', "$co";
+ echo $select,' ', $skad,$gdzie, ' ','kolor like ',$kolor ,' and ', 'marka like ', "$co";
 $all = ob_get_contents();
 ob_end_clean();
 }
 else{
 	ob_start();
- echo $select,' ', $skad,$gdzie, ' ','kategoria like ',$kategoria , ' and cena ',$cena, ' ', $cena1,' and ', 'nazwa like ', "$co";
+ echo $select,' ', $skad,$gdzie, ' ','kolor like ',$kolor , ' and ',$cena,' and ', 'marka like ', "$co", ' and ',$cena;
 $all = ob_get_contents();
 ob_end_clean();
 };
-echo $all;
+//echo $all;
 
 
 
 	
-    $query2 = mysqli_query($conn, $all);
+    $query2 = mysqli_query($conn, $all); // zapytanie
     $times = mysqli_num_rows($query2);
 
-        for ($i=0; $i < $times; $i++) { 
+        for ($i=0; $i < $times; $i++) {  //wykonuje tyle razy ile powyżej zwrócił wyników dla każdego wiersza
 	$forrow = mysqli_fetch_row($query2);
 	echo '</br>';
 	foreach ($forrow as $key) {
