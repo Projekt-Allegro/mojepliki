f34ad42c4f6b4ae8331d85f5522e298f2e7d434b
likearockets <aczech81@gmail.com>
2017-11-16 18:44:21 +0100

    to jest to co daje w boxa tylko dane z bazy

---
+++ test.php
@@ -0,0 +1,20 @@
+﻿<?php
+$con=mysqli_connect("localhost","root","","komis");
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+  
+$sql="select distinct marka FROM auto"; 
+if ($result=mysqli_query($con,$sql))
+  {
+  echo '<select name="nazwa" size="1">';
+  while ($row=mysqli_fetch_assoc($result)){
+    echo '<option value="'. $row["marka"] .'">'. $row["marka"] .'</option>';
+    }
+	echo '</select>';
+	
+  mysqli_free_result($result);
+}
+mysqli_close($con);
+?>
